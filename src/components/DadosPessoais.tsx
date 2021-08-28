e9fc34611897a915e24ae7ba6fd95be0ebee6293
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2021-08-28 13:53:07 -0300

    att cadCleitne

--- src/components/DadosPessoais.tsx
+++ src/components/DadosPessoais.tsx
@@ -1,15 +1,24 @@
-import { useContext, useState, FormEvent, ChangeEvent } from 'react';
-import { useHistory } from 'react-router-dom';
-import { Context } from '../contexts/AuthContext';
-import '../styles/dadosPessoais.scss'
-import api from '../api';
+import { useContext, useState, FormEvent, ChangeEvent, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { Context } from "../contexts/AuthContext";
+import "../styles/dadosPessoais.scss";
+import api from "../api";
 
-export function DadosPessoais() {
-    const history = useHistory();
-    const { handleLogin } = useContext(Context);
-    const [cliente, setCliente] = useState({ "cli_senha": '', "cli_confsenha": '' });
+type cliProps = {
+  callback: Function;
+};
 
-    /*  "cli_pnome": pnome,
+export function DadosPessoais(props: cliProps) {
+  const history = useHistory();
+  const { handleLogin } = useContext(Context);
+  const [cliente, setCliente] = useState({
+    cli_senha: "",
+    cli_confsenha: "",
+    cli_sexo: "Masculino",
+    type: "cliente"
+  });
+
+  /*  "cli_pnome": pnome,
         "cli_unome": unome,
         "cli_rg": rg,
         "cli_cpf": cpf,
@@ -17,60 +26,121 @@ export function DadosPessoais() {
         "cli_telefone": telefone,
         "cli_senha": password, */
 
-    async function insertClientForm(e: FormEvent) {
-        e.preventDefault();
-        if (cliente.cli_senha === cliente.cli_confsenha) {
+  useEffect(() => {
+    props.callback(cliente);
+    console.log(cliente);
+  }, [cliente]);
 
-        } else {
-            alert("senhas vefirique a confirmação da senha!")
-        }
-        handleLogin(cliente.cli_senha, cliente.cli_confsenha);
-        history.push('/cadastroCliente');
+  async function insertClientForm(e: FormEvent) {
+    e.preventDefault();
+    if (cliente.cli_senha === cliente.cli_confsenha) {
+    } else {
+      alert("senhas vefirique a confirmação da senha!");
     }
+    handleLogin(cliente.cli_senha, cliente.cli_confsenha);
+    history.push("/cadastroCliente");
+  }
 
-    function handleCliente(e: ChangeEvent<HTMLInputElement>) {
-        const value = e.target.value;
-        setCliente({
-            ...cliente,
-            [e.target.name]: value
-        });
-        console.log(cliente)
-    }
+  function handleCliente(e: any) {
+    const value = e.target.value;
+    setCliente({
+      ...cliente,
+      [e.target.name]: value
+    });
+  }
 
-    return (
-        <div className="dados__pessoais">
-            <div className="dados__pessoais__nome">
-                <input name="cli_pnome" onChange={e => handleCliente(e)} type="text" placeholder="Primeiro Nome" />
-                <input name="cli_unome" onChange={e => handleCliente(e)} type="text" placeholder="Ultimo Nome" />
-            </div>
-            <div className="dados__pessoais__pessoa">
-                    <label htmlFor="cli_dtnascimento">Data de nascimento</label>
-                    <label htmlFor="cli_rg">RG</label>
-                    <label htmlFor="cli_cpf">CPF</label>
-                    <label htmlFor="cli_sexo">Sexo</label>
-                    <input name="cli_dtnascimento" onChange={e => handleCliente(e)} type="date" placeholder="" />
-                    <input name="cli_rg" onChange={e => handleCliente(e)} type="text" placeholder="RG" />
-                    <input name="cli_cpf" onChange={e => handleCliente(e)} type="text" placeholder="CPF" />
-                    <select name="cli_sexo" id="">
-                        <option value="masculino">Masculino</option>
-                        <option value="feminino">Feminino</option>
-                        <option value="outro">Outro</option>
-                    </select>
-            </div>
-            <div className="dados__pessoais__outros">
-                <label htmlFor="cli_email">Seu melhor email</label>
-                <label htmlFor="cli_telefone">Seu numero de telefone</label>
-                <input name="cli_email" onChange={e => handleCliente(e)} type="email" placeholder="Email" />
-                <input name="cli_telefone" onChange={e => handleCliente(e)} type="tel" placeholder="Telefone" />
-            </div>
-            <div className="dados__pessoais__usuario">
-                <label htmlFor="cli_senha">Senha</label>
-                <label htmlFor="cli_confsenha">Confirme a senha</label>
-                <label htmlFor=""></label>
-                <input name="cli_senha" onChange={e => handleCliente(e)} type="password" placeholder="Senha" className="senha"/>
-                <input name="cli_confsenha" onChange={e => handleCliente(e)} type="password" placeholder="Confirmar Senha" className="confsenha"/>
-            </div>
-        </div>
-
-    )
+  return (
+    <div className="dados__pessoais">
+      <div className="dados__pessoais__nome">
+        <input
+          name="cli_pnome"
+          onChange={(e) => handleCliente(e)}
+          type="text"
+          placeholder="Primeiro Nome"
+        />
+        <input
+          name="cli_unome"
+          onChange={(e) => handleCliente(e)}
+          type="text"
+          placeholder="Ultimo Nome"
+        />
+      </div>
+      <div className="dados__pessoais__pessoa">
+        <label htmlFor="cli_dtnascimento">Data de nascimento</label>
+        <label htmlFor="cli_rg">RG</label>
+        <label htmlFor="cli_cpf">CPF</label>
+        <label htmlFor="cli_sexo">Sexo</label>
+        <input
+          name="cli_dtnascimento"
+          onChange={(e) => handleCliente(e)}
+          type="date"
+          placeholder=""
+        />
+        <input
+          name="cli_rg"
+          onChange={(e) => handleCliente(e)}
+          type="text"
+          placeholder="RG"
+        />
+        <input
+          name="cli_cpf"
+          onChange={(e) => handleCliente(e)}
+          type="text"
+          placeholder="CPF"
+        />
+        <select
+          name="cli_sexo"
+          id=""
+          onChange={(e: any) => handleCliente(e)}
+          value={cliente.cli_sexo}
+        >
+          <option value="masculino">Masculino</option>
+          <option value="feminino">Feminino</option>
+          <option value="outro">Outro</option>
+        </select>
+      </div>
+      <div className="dados__pessoais__outros">
+        <label htmlFor="cli_email">Seu melhor email</label>
+        <label htmlFor="cli_ddd">DDD</label>
+        <label htmlFor="cli_telefone">Seu numero de telefone</label>
+        <input
+          name="cli_email"
+          onChange={(e) => handleCliente(e)}
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          name="cli_ddd"
+          onChange={(e) => handleCliente(e)}
+          type="number"
+          placeholder="DDD"
+        />
+        <input
+          name="cli_telefone"
+          onChange={(e) => handleCliente(e)}
+          type="number"
+          placeholder="Telefone"
+        />
+      </div>
+      <div className="dados__pessoais__usuario">
+        <label htmlFor="cli_senha">Senha</label>
+        <label htmlFor="cli_confsenha">Confirme a senha</label>
+        <label htmlFor=""></label>
+        <input
+          name="cli_senha"
+          onChange={(e) => handleCliente(e)}
+          type="password"
+          placeholder="Senha"
+          className="senha"
+        />
+        <input
+          name="cli_confsenha"
+          onChange={(e) => handleCliente(e)}
+          type="password"
+          placeholder="Confirmar Senha"
+          className="confsenha"
+        />
+      </div>
+    </div>
+  );
 }
--- src/pages/CadastroCliente.tsx
+++ src/pages/CadastroCliente.tsx
@@ -1,85 +1,120 @@
-import { useContext, useState, FormEvent } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useContext, useState, FormEvent, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
-import { Footer } from '../components/Footer';
-import { Header } from '../components/Header';
-import { Form } from '../components/Form';
-import { Endereco } from '../components/Endereco';
-import { DadosPessoais } from '../components/DadosPessoais';
-import { Context } from '../contexts/AuthContext';
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import { Form } from "../components/Form";
+import { Endereco } from "../components/Endereco";
+import { DadosPessoais } from "../components/DadosPessoais";
+import { Context } from "../contexts/AuthContext";
 
-import api from '../api';
-import ReactDOM from 'react-dom';
+import api from "../api";
+import ReactDOM from "react-dom";
 
 export function CadastroCliente() {
-    const history = useHistory();
-    const { handleLogin } = useContext(Context);
-    const [enderecos, setEnderecos] = useState([{}]);
-    const [cliente, setCliente] = useState({});
+  const history = useHistory();
+  const { handleLogin } = useContext(Context);
+  const [enderecos, setEnderecos] = useState([{}]);
+  const [cliente, setCliente] = useState({});
 
-    const tiposExigidos = ["cobranca","entrega"];
+  const tiposExigidos = ["cobranca", "entrega"];
 
-    function enderecosHandler(endereco: Object, key: number) {
-        let temp_enderecos = enderecos;
-        temp_enderecos[key] = endereco;
-        setEnderecos(temp_enderecos);
-        console.log("Enderecos:", enderecos)
+  useEffect(() => {
+    console.log("Search message inside useEffect: ", enderecos);
+  }, [enderecos]);
+
+  function enderecosHandler(endereco: Object, key: number) {
+    let temp_enderecos = enderecos;
+    temp_enderecos[key] = endereco;
+    setEnderecos(temp_enderecos);
+  }
+
+  function clienteHandler(cliente: Object) {
+    setCliente(cliente);
+  }
+
+  async function postCliente(e: FormEvent) {
+    e.preventDefault();
+    let validaTipos = tiposExigidos;
+    enderecos.forEach((item) => {
+      if (item["end_tipo" as keyof typeof item] === "ambos") validaTipos = [];
+      let tipoIndex = validaTipos.indexOf(
+        item["end_tipo" as keyof typeof item]
+      );
+      if (tipoIndex >= 0) validaTipos.splice(tipoIndex, 1);
+    });
+    if (Object.keys(validaTipos).length != 0) {
     }
+    console.log(cliente);
+    const request = await api.post("/clientes/insert", cliente);
+    console.log(request);
+  }
 
-    async function postCliente(e:FormEvent) {
-        e.preventDefault();
-        let validaTipos = tiposExigidos;
-        enderecos.forEach((item) => {
-            if(item["end_tipo" as keyof typeof item]==="ambos") validaTipos = []
-            let tipoIndex = validaTipos.indexOf(item["end_tipo" as keyof typeof item])
-            if(tipoIndex>=0) validaTipos.splice(tipoIndex,1);
-        })
-        if (Object.keys(validaTipos).length != 0) {
-            console.log(validaTipos);
-        }
-    }
+  function addEnderecos() {
+    var temp_end = enderecos;
+    temp_end.push({});
+    setEnderecos([...temp_end]);
 
-    function addEnderecos() {
-        var temp_end = enderecos;
-        temp_end.push({});
-        setEnderecos([...temp_end]);
+    var k = document.getElementsByClassName("enderecos__form");
+  }
 
-        var k = document.getElementsByClassName('enderecos__form')
-    }
+  function rmEnderecos() {
+    var temp_end = enderecos;
+    temp_end.pop();
+    setEnderecos([...temp_end]);
+  }
 
-    function rmEnderecos() {
-        var temp_end = enderecos;
-        temp_end.pop();
-        setEnderecos([...temp_end]);
-    }
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container">
+          <Form
+            submitFunction={(e: FormEvent) => postCliente(e)}
+            title="Cadastre-se"
+            about="Usuário"
+            buttonText="Criar conta"
+            modalMessage="Cadastrado com sucesso"
+          >
+            <DadosPessoais
+              callback={(e: Object) => {
+                clienteHandler(e);
+              }}
+            />
+            <hr></hr>
+            <h1 className="cliente__form__enderecoIndicator form__title">
+              Enderecos
+            </h1>
+            <div className="form__body__btngroup">
+              <button
+                type="button"
+                className="form__body__button"
+                onClick={(e) => addEnderecos()}
+              >
+                Adicionar
+              </button>
+              <button
+                type="button"
+                className="form__body__button"
+                onClick={(e) => rmEnderecos()}
+              >
+                Remover
+              </button>
+            </div>
 
-    return (
-        <>
-            <Header />
-            <main>
-                <div className="container">
-                    <Form
-                        submitFunction={(e:FormEvent) => postCliente(e)}
-                        title="Cadastre-se"
-                        about="Usuário"
-                        buttonText="Criar conta"
-                        modalMessage="Cadastrado com sucesso"
-                    >
-                        <DadosPessoais />
-                        <hr></hr>
-                        <h1 className="cliente__form__enderecoIndicator form__title">Enderecos</h1>
-                        <div className="form__body__btngroup">
-                            <button type="button" className="form__body__button" onClick={(e) => addEnderecos()}>Adicionar</button>
-                            <button type="button" className="form__body__button" onClick={(e) => rmEnderecos()}>Remover</button>
-                        </div>
-                        
-                        {enderecos.map((itens, index) => (
-                            <Endereco key={index} index={index} callback={(e: Object) => { enderecosHandler(e, index) }} />
-                        ))}
-                    </Form>
-                </div>
-            </main>
-            <Footer />
-        </>
-    )
+            {enderecos.map((itens, index) => (
+              <Endereco
+                key={index}
+                index={index}
+                callback={(e: Object) => {
+                  enderecosHandler(e, index);
+                }}
+              />
+            ))}
+          </Form>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
